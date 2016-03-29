298f9bd52ab7be382eacfbdd26d42e777082d398
RobStek <rob.stekelenburg@idgis.nl>
2016-03-29 17:19:47 +0200

    added tabels result_info and request_info

--- db/create_db.sql
+++ db/create_db.sql
@@ -3,3 +3,22 @@ create database download;
 \c download
 
 create table test(id serial, test text);
+
+create tabel result_info(
+	id serial,
+	request_id text,
+	response_time timestamp;
+	response_code text
+);
+
+create tabel request_info(
+	id serial,
+	request_id text,
+	request_time timestamp,
+	download text,
+	uuid text,
+	user_name text,
+	user_emailaddress text,
+	user_format text
+	
+);
