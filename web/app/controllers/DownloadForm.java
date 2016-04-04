b492cbee0849d85312106e97936af7e5c63a11fa
copierrj <reijer.copier@idgis.nl>
2016-04-04 14:13:40 +0200

    metadata module merged into web module

--- web/app/controllers/DownloadForm.java
+++ web/app/controllers/DownloadForm.java
@@ -3,8 +3,13 @@ package controllers;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 import javax.inject.Inject;
+import javax.xml.XMLConstants;
+import javax.xml.namespace.QName;
+
+import data.MetadataProvider;
 
 import models.DownloadInfo;
 import models.DownloadRequest;
@@ -13,17 +18,27 @@ import models.OutputFormat;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.libs.F.Promise;
 
 import views.html.form;
 import views.html.help;
 
+import nl.idgis.downloadtool.domain.Download;
+import nl.idgis.downloadtool.domain.WfsFeatureType;
+import nl.idgis.downloadtool.domain.AdditionalData;
+
 public class DownloadForm extends Controller {
+	
+	private final static String STYLESHEET = "datasets/intern/metadata.xsl";
 	
 	private final WebJarAssets webJarAssets;
 	
+	private final MetadataProvider metadataProvider;
+	
 	@Inject
-	public DownloadForm(WebJarAssets webJarAssets) {
+	public DownloadForm(WebJarAssets webJarAssets,  MetadataProvider metadataProvider) {
 		this.webJarAssets = webJarAssets;
+		this.metadataProvider = metadataProvider;
 	}
 	
 	private DownloadInfo info(String id) {
@@ -46,21 +61,58 @@ public class DownloadForm extends Controller {
 			Form.form(DownloadRequest.class)));
 	}
 	
-	public Result post(String id) {
+	public Promise<Result> post(String id) {
 		Form<DownloadRequest> downloadForm = 
 			Form.form(DownloadRequest.class).bindFromRequest();
 		
 		if(downloadForm.hasErrors()) {
-			return badRequest(form.render(
-				webJarAssets,
-				id,
-				info(id), 
-				downloadForm));
+			return Promise.pure(
+				badRequest(form.render(
+					webJarAssets,
+					id,
+					info(id), 
+					downloadForm)));
 		}
 		
 		DownloadRequest request = downloadForm.get();
 		
-		return ok("form ok");
+		return metadataProvider.get(id).map(document -> {
+			if(!document.isPresent()) {
+				return notFound();
+			}
+
+			List<AdditionalData> additionalData = new ArrayList<>();
+
+			AdditionalData stylesheet = new AdditionalData();
+			stylesheet.setName("metadata");
+			stylesheet.setExtension("xsl");
+			stylesheet.setUrl(routes.WebJarAssets.at(webJarAssets.locate(STYLESHEET)).absoluteURL(request()));
+			additionalData.add(stylesheet);
+
+			AdditionalData metadata = new AdditionalData();
+			metadata.setName(id);
+			metadata.setExtension("xml");
+			metadata.setUrl(routes.Metadata.get(id).absoluteURL(request()));
+			additionalData.add(metadata);
+
+			WfsFeatureType ft = new WfsFeatureType();
+			ft.setServiceUrl(document.get().getWFSUrl());
+
+			QName featureTypeName = document.get().getFeatureTypeName();
+			String namespaceURI = featureTypeName.getNamespaceURI();
+
+			ft.setName(featureTypeName.getLocalPart());
+			if(!XMLConstants.NULL_NS_URI.equals(featureTypeName.getNamespaceURI())) {
+				ft.setNamespaceUri(namespaceURI);
+			}
+
+			Download download = new Download();
+			download.setName(id);
+			download.setFt(ft);
+			download.setAdditionalData(additionalData);
+
+			return ok("job created");
+		});
 	}
 	
 	public Result help() {
