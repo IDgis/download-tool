faeb617537edd000d52aef431786942ff2d22f37
RobStek <rob.stekelenburg@idgis.nl>
2016-04-08 15:04:56 +0200

    clean import

--- dao/src/main/java/nl/idgis/downloadtool/dao/DownloadDao.java
+++ dao/src/main/java/nl/idgis/downloadtool/dao/DownloadDao.java
@@ -4,7 +4,6 @@
 package nl.idgis.downloadtool.dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
