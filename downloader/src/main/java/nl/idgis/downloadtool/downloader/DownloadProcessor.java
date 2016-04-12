a52d44e6fa2d4ed89a044cb55161a65cfce137d8
copierrj <reijer.copier@idgis.nl>
2016-04-12 09:31:13 +0200

    default values for environment variables removed

--- downloader/src/main/java/nl/idgis/downloadtool/downloader/DownloadProcessor.java
+++ downloader/src/main/java/nl/idgis/downloadtool/downloader/DownloadProcessor.java
@@ -251,29 +251,24 @@ public class DownloadProcessor {
 			fromStream.reset();
 		return markSupported;
 	}
+	
+	private static String getEnv(String name) {
+		String value = System.getenv(name);
+		
+		if(value == null) {
+			throw new IllegalArgumentException(name + " environment variable is missing");
+		}
+			
+		return value;
+	}
 
 	public static void main(String... args) {
-		String path = System.getenv("ZIP_CACHEPATH");
-		if (path == null) {
-			path = System.getProperty("user.home");
-		}
-		String host = System.getenv("BEANSTALK_HOST");
-		if (host == null) {
-			host = "localhost";
-		}
-		String downloadQueueTubeName = System.getenv("BEANSTALK_DOWNLOAD_QUEUE");
-		if (downloadQueueTubeName == null) {
-			downloadQueueTubeName = "downloadRequestTube";
-		}
-		String feedbackOkTubeName = System.getenv("BEANSTALK_FEEDBACKOK_QUEUE");
-		if (feedbackOkTubeName == null) {
-			feedbackOkTubeName = "feedbackOkTube";
-		}
-		String feedbackErrorTubeName = System.getenv("BEANSTALK_FEEDBACKERROR_QUEUE");
-		if (feedbackErrorTubeName == null) {
-			feedbackErrorTubeName = "feedbackErrorTube";
-		}
-
+		String path = getEnv("ZIP_CACHEPATH");		
+		String host = getEnv("BEANSTALK_HOST");
+		String downloadQueueTubeName = getEnv("BEANSTALK_DOWNLOAD_QUEUE");
+		String feedbackOkTubeName = getEnv("BEANSTALK_FEEDBACKOK_QUEUE");
+		String feedbackErrorTubeName = getEnv("BEANSTALK_FEEDBACKERROR_QUEUE");
+		
 		try {
 			log.info("start loop " + path);
 			DownloadProcessor dlp = new DownloadProcessor(path);
